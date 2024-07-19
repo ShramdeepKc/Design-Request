c5c53c041cbe4581b0bb773cc5a8978089a827fa
Bipin Khanal <bkhanal@dfwitpartner.com>
2024-07-19 17:42:31 +0545

    feat: add breadcrumb

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 from odoo import http
-from odoo.addons.portal.controllers.portal import CustomerPortal as CustomerPortal
+from odoo.addons.portal.controllers.portal import CustomerPortal
 from odoo.http import request, route
 
 class CustomerPortalHome(CustomerPortal):
-    @route(['/my', '/my/home'], type='http', auth="user", website=True)
-    def home(self, **kw):
-        values = self._prepare_portal_layout_values()
-        return request.render("design_request.design_home", values)
 
     @route(['/my/designs'], type='http', auth="user", website=True)
     def lists(self, **kw):
-        values = self._prepare_portal_layout_values()
+        values = {'values': self._prepare_portal_layout_values(), 'page_name': 'design_lists'}
         return request.render("design_request.design_lists", values)
+
 
