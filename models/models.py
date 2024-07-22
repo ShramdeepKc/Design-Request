750ab63a78392d1b2e50e2ae7d8b024da82d1433
Bipin Khanal <bkhanal@dfwitpartner.com>
2024-07-22 14:42:37 +0545

    feat: add server side validation

--- models/models.py
+++ models/models.py
@@ -9,5 +9,5 @@ class design_request(models.Model):
 
     design_name = fields.Char(string='Design Name')
     customer_email = fields.Char(string='Customer Email')
-    design_image = fields.Image(string='Design Image', attachment=True, max_width=100, max_height=100)
+    design_image = fields.Image(string='Design Image', attachment=True)
 
